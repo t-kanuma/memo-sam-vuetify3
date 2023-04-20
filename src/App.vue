d090e8af400ac681e529569e8a30a992686c3702
“Takahiro <takahiro-kanuma@gmogshd.com>
2023-04-20 11:45:55 +0900

    done js version.

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,8 @@
 @import url("https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap");
 
 .v-application {
-  font-family: "Helvetica Neue", "Helvetica", "Hiragino Maru Gothic ProN",
-    "Hiragino Sans", "Hiragino Kaku Gothic ProN", "Arial", "Yu Gothic", "Meiryo",
-    sans-serif;
+  font-family: "Noto Sans JP", "Helvetica Neue", "Helvetica", "Hiragino Sans",
+    "Hiragino Kaku Gothic ProN", "Arial", "Yu Gothic", "Meiryo", sans-serif;
 }
 
 a {
